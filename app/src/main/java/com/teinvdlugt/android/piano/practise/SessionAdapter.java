ce3a4c9f912c562e3f8603c9d792f34a320b532f
Tein van der Lugt <teinvdlugt@gmail.com>
2017-03-27 20:40:26 +0200

    Show dialog on session click

--- app/src/main/java/com/teinvdlugt/android/piano/practise/SessionAdapter.java
+++ app/src/main/java/com/teinvdlugt/android/piano/practise/SessionAdapter.java
@@ -1,6 +1,7 @@
 package com.teinvdlugt.android.piano.practise;
 
 import android.content.Context;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -45,16 +46,35 @@ class SessionAdapter extends RecyclerView.Adapter<SessionAdapter.ViewHolder> {
         return data == null ? 0 : data.size();
     }
 
+    private void showDialog(Session session) {
+        DateFormat format = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.SHORT);
+        new AlertDialog.Builder(context)
+                .setMessage(context.getString(R.string.session_format,
+                        format.format(new Date(session.getTimestamp())),
+                        session.getDuration() / 60000, session.getDescription()))
+                .setPositiveButton(R.string.ok, null)
+                .create().show();
+    }
+
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView textView1, textView2;
+        Session currentData;
 
         ViewHolder(View itemView) {
             super(itemView);
             textView1 = (TextView) itemView.findViewById(R.id.textView1);
             textView2 = (TextView) itemView.findViewById(R.id.textView2);
+            itemView.findViewById(R.id.root)
+                    .setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (currentData != null) showDialog(currentData);
+                        }
+                    });
         }
 
         void bind(Session data) {
+            currentData = data;
             textView1.setText(context.getString(R.string.practiseDurationMinutes_format,
                     data.getDuration() / 60000));
             textView2.setText(dateFormat.format(new Date(data.getTimestamp())));
